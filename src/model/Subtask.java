2c43f0f9c9e6fcb61ea6fa2dc015dbe12aa1000a
microgoose <gusakmachine@yandex.ru>
2024-08-11 15:56:14 +0300

    fix(Subtask): стилизация кода

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -11,7 +11,7 @@ public class Subtask extends Task {
     public Subtask(Integer id, String name, String description, TaskStatus status) {
         this(id, name, description, status, null);
     }
-    
+
     public Subtask(Integer id, String name, String description) {
         this(id, name, description, TaskStatus.NEW, null);
     }
@@ -19,11 +19,11 @@ public class Subtask extends Task {
     public Subtask(Integer id, String name, String description, Integer epicId) {
         this(id, name, description, TaskStatus.NEW, epicId);
     }
-    
+
     public Subtask(String name, String description, TaskStatus status) {
         this(null, name, description, status, null);
     }
-    
+
     public Subtask(String name, String description) {
         this(null, name, description, TaskStatus.NEW, null);
     }
@@ -31,7 +31,7 @@ public class Subtask extends Task {
     public Subtask(String name, String description, Integer epicId) {
         this(null, name, description, TaskStatus.NEW, epicId);
     }
-    
+
     public Integer getEpicId() {
         return epicId;
     }
@@ -43,7 +43,7 @@ public class Subtask extends Task {
     @Override
     public String toString() {
         //3,SUBTASK,Sub Task2,DONE,Description sub task3,2
-        String epicId = getEpicId() == null? "" : getEpicId().toString();
+        String epicId = getEpicId() == null ? "" : getEpicId().toString();
         return String.format("%s,%S,%s,%S,%s,%s", getId(), TaskTypes.SUBTASK, getName(), getStatus(), getDescription(), epicId);
     }
 
@@ -52,20 +52,20 @@ public class Subtask extends Task {
 
         if (parts.length == 6) {
             return new Subtask(
-                Integer.parseInt(parts[0]),
-                parts[2],
-                parts[4],
-                TaskStatus.valueOf(parts[3]),
-                Integer.parseInt(parts[5])
+                    Integer.parseInt(parts[0]),
+                    parts[2],
+                    parts[4],
+                    TaskStatus.valueOf(parts[3]),
+                    Integer.parseInt(parts[5])
             );
         }
 
         if (parts.length == 5) {
             return new Subtask(
-                Integer.parseInt(parts[0]),
-                parts[2],
-                parts[4],
-                TaskStatus.valueOf(parts[3])
+                    Integer.parseInt(parts[0]),
+                    parts[2],
+                    parts[4],
+                    TaskStatus.valueOf(parts[3])
             );
         }
 
